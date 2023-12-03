66cac0f314bb4fc7f2b1e721146bc9fc496c89d3
Svygzhryr <pamixy@gmail.com>
2023-12-03 21:54:57 +0300

    other: change navbar and netlify

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,13 +6,19 @@ export const Navbar = () => {
   return (
     <div className={styles.navbarWrapper}>
       <Link to="/controlled">
-        <button className={`${styles.button} ${styles.signup}`}>Sign the Contract</button>
+        <button className={`${styles.button} ${styles.signup}`}>
+          Controlled
+          <p className={styles.controlledText}>Sign the contract</p>
+        </button>
       </Link>
       <Link className={styles.logo} to="/">
         Sweet<span className={styles.soul}>Soul</span>
       </Link>
       <Link to="/uncontrolled">
-        <button className={styles.button}>Terminate the Contract</button>
+        <button className={styles.button}>
+          Uncontrolled
+          <p className={styles.uncontrolledText}>Terminate the contract</p>
+        </button>
       </Link>
     </div>
   );
